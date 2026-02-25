87b2a4fe3cdb1ea48a6685dd482e1b034e0e6086
beatriz-reis <anabeatriz.reis@ort.org.br>
2026-02-25 15:52:02 -0300

    exercicio 3 completo

---
+++ #exercicio3.py
@@ -0,0 +1,18 @@
+# exercicio 3
+#  Desenvolva um programa para calcular e escrever a área e o perímetro de um retângulo.
+#exemplo
+Base= 5
+Altura = 4
+Área = 20
+Perímetro = 18
+
+#entrada
+Base = int(input('informe a Base: '))
+Altura = int(input('informe a Altura: '))
+Área  = int(input('informe a Área: '))
+
+#processamento
+Perímetro = Base + Altura + Área 
+
+#saida
+print('perimetro:', Perímetro)
