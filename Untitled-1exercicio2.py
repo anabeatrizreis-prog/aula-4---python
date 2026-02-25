0bc3d5e79dfda28e4ba5d731efe2a862ac60e41c
beatriz-reis <anabeatriz.reis@ort.org.br>
2026-02-25 15:23:47 -0300

    exercicio 2

---
+++ Untitled-1exercicio2.py
@@ -0,0 +1,13 @@
+# exercicio 2
+
+# entrada
+n1 = float(input('informe a nota 01: '))
+n2 = float(input('informe a nota 02: '))
+n3 = float(input('informe a nota 03: '))
+n4 = float(input('informe a nota 04: '))
+
+# processamento
+media = (n1 + n2 + n3 + n4) / 4
+
+# saida
+print('media final:', media)
